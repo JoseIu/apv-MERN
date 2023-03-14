1cca8989660cf5b258720a70210897042a513ee5
Jose luis <boxixz0991@gmail.com>
2023-03-14 11:49:04 +0100

    fixing bcrypt

--- backend/routes/veterinaryRoutes.js
+++ backend/routes/veterinaryRoutes.js
@@ -10,7 +10,6 @@ veterinaryRouter.post('/login', loging);
 
 veterinaryRouter.post('/olvide-password', resetPassword);
 veterinaryRouter.route('/olvide-password/:token').get(checkToken).post(newPwd);
-const a = 'a';
 
 //Area privada
 // middleware checkAuth para verificar/proteger la ruta profile
